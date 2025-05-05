788ccea1df79a786e855b7c91058de3375affb02
Asim Ansari <152499370+screenprog@users.noreply.github.com>
2025-05-06 00:37:28 +0530

    now emails are well formatted; changed prompt and model

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import os
-import smtplib
-import time
-from datetime import datetime
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
 
 import httpx
 from google import genai
 from google.genai import types
 from dotenv import load_dotenv
 from pymongo.mongo_client import MongoClient
+
+from datetime import datetime
 import pytz
 
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 load_dotenv()
 
 SENDER_EMAIL_ADDRESS = os.getenv("SENDER_EMAIL_ADDRESS")
@@ -22,19 +22,22 @@ EMAIL_APP_PASS = os.getenv("EMAIL_APP_PASS")
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 MONGODB_URI = os.getenv("MONGODB_URI")
 DATABASE = os.getenv("DATABASE")
-COLLECTION = os.getenv("COLLECTION")
-EMAIL_TOOL_DESCRIPTION = os.getenv("EMAIL_TOOL_DESCRIPTION")
-MODEL_NAME = os.getenv("MODEL_NAME")
+COLLECTION = os.getenv("NEW_COLLECTION")
+MODEL_NAME = os.getenv("NEW_MODEL")
+
 tz = pytz.timezone(os.getenv("TIME_ZONE"))
 
-def sendEmail(subject:str, body:str):
+import markdown
+def send_email(subject:str, body:str):
     load_dotenv()
     try:
+        html_body = markdown.markdown(body)
         msg = MIMEMultipart()
         msg['From'] = SENDER_EMAIL_ADDRESS
         msg['To'] = RECEIVER_EMAIL_ADDRESS
         msg['Subject'] = subject
-        msg.attach(MIMEText(body, 'plain'))
+
+        msg.attach(MIMEText(html_body, 'html'))
 
         with smtplib.SMTP(SMTP_SERVER, SMTP_PORT) as server:
             server.starttls()
@@ -79,38 +82,40 @@ def get_chat_history():
     client.close()
     return chat_content
 
-def generate(chat_history):
-    client = genai.Client(api_key=GEMINI_API_KEY)
+
+def generate(contents):
+    client = genai.Client(
+        api_key=GEMINI_API_KEY,
+    )
+    model = MODEL_NAME
     tools = [
         types.Tool(
             function_declarations=[
                 types.FunctionDeclaration(
-                    name="sendEmail",
-                    description=EMAIL_TOOL_DESCRIPTION,
+                    name="send_email",
+                    description="sends email to end user",
                     parameters=genai.types.Schema(
-                        type=genai.types.Type.OBJECT,
-                        properties={
+                        type = genai.types.Type.OBJECT,
+                        properties = {
                             "subject": genai.types.Schema(
-                                type=genai.types.Type.STRING
+                                type = genai.types.Type.STRING,
                             ),
                             "body": genai.types.Schema(
-                                type=genai.types.Type.STRING
-                            )
-                        }
-                    )
-                )
-            ]
-        )
+                                type = genai.types.Type.STRING,
+                            ),
+                        },
+                    ),
+                ),
+            ])
     ]
-
-    model = MODEL_NAME
-    contents = chat_history
     generate_content_config = types.GenerateContentConfig(
+        temperature=0.9,
         tools=tools,
         response_mime_type="text/plain",
         system_instruction=[
-            types.Part.from_text(text=os.getenv("SYSTEM_PROMPT")),
+            types.Part.from_text(text=os.getenv("NEW_PROMPT")),
         ],
+        thinking_config=types.ThinkingConfig(thinking_budget=1024)
     )
 
     return client.models.generate_content(
@@ -119,54 +124,50 @@ def generate(chat_history):
         config=generate_content_config,
     )
 
-def day_ordinals():
-    number = int(datetime.now(tz).strftime(f"%d"))
-    if 3 < number < 21:
-        return str(number) + "th"
-    remainder = number%10
-    return str(number) + ('st' if remainder==1 else ( 'nd' if remainder==2 else ( 'rd' if remainder==3 else 'th' )) )
-
 def set_chat_history(message: dict):
     client = MongoClient(MONGODB_URI)
     collection = client.get_database(DATABASE).get_collection(COLLECTION)
     collection.insert_one(message)
     client.close()
 
-def main():
+
+if __name__ == "__main__":
     chat_messages = get_chat_history()
     print("acquired the data.")
     try:
-        inputText = ""
-        while inputText != "bye":
-            inputText = datetime.now(tz).strftime(f"It's %A, {day_ordinals()} %B, %Y; %H:%M:%S")
-            set_chat_history({"user": inputText, "time": datetime.now(tz)})
-            chat_messages.append(types.Content(role="user", parts=[types.Part.from_text(text=inputText)]))
+        # inputText =  input(":")
+        inputText =  "SEND_MORNING" if int(datetime.now(tz).strftime("%H")) <= 12 else "SEND_EVENING"
+        set_chat_history({"user": inputText, "time": datetime.now(tz)})
+        chat_messages.append(types.Content(role="user", parts=[types.Part.from_text(text=inputText)]))
+        response = generate(chat_messages)
+        if response.function_calls is None:
+            print(f"Model Response: {response.text}")
+            chat_messages.append(types.Content(role="model", parts=[types.Part.from_text(text=response.text)]))
+            set_chat_history({"model": response.text, "time": datetime.now(tz)})
+        while response.function_calls is not None:
+            function_response = {"result": send_email(**response.function_calls[0].args)}
+            function_name = response.function_calls[0].name
+            print("model called the function.")
+            chat_messages.append(types.Content(role="model", parts=[
+                types.Part.from_function_call(name=function_name,
+                                              args=response.function_calls[0].args)]))
+            set_chat_history(
+                    {"function": {"name": response.function_calls[0].name, "args": response.function_calls[0].args},
+                     "time": datetime.now(tz)})
+            print("function calling ended.")
+            chat_messages.append(types.Content(role="function", parts=[
+                types.Part.from_function_response(name=response.function_calls[0].name,
+                                                  response=function_response)]))
+            set_chat_history(
+                {"function_response": {"name": response.function_calls[0].name, "response": function_response},
+                 "time": datetime.now(tz)})
+            print("model received function response.")
             response = generate(chat_messages)
             if response.function_calls is None:
-                print(f"Response To Time: {response.text}")
+                print(f"Response After Function Call: {response.text}")
                 chat_messages.append(types.Content(role="model", parts=[types.Part.from_text(text=response.text)]))
                 set_chat_history({"model": response.text, "time": datetime.now(tz)})
-            while response.function_calls is not None:
-                function_response = {"result": sendEmail(**response.function_calls[0].args)}
-                function_name = response.function_calls[0].name
-                print("model called the function.")
-                chat_messages.append(types.Content(role="model", parts=[
-                    types.Part.from_function_call(name=function_name,
-                                                  args=response.function_calls[0].args)]))
-                set_chat_history({"function": {"name": response.function_calls[0].name, "args": response.function_calls[0].args}, "time": datetime.now(tz)})
-                print("function calling ended.")
-                chat_messages.append(types.Content(role="function", parts=[
-                    types.Part.from_function_response(name=response.function_calls[0].name, response=function_response)]))
-                set_chat_history({"function_response": {"name": response.function_calls[0].name, "response": function_response}, "time": datetime.now(tz)})
-                print("model received function response.")
-                response = generate(chat_messages)
-                if response.function_calls is None:
-                    print(f"Response After Function Call: {response.text}")
-                    chat_messages.append(types.Content(role="model", parts=[types.Part.from_text(text=response.text)]))
-                    set_chat_history({"model": response.text, "time": datetime.now(tz)})
 
-                print("responded to function")
-            #talk_or_inform()
-            time.sleep(7200) # Two hours
+            print("responded to function")
     except httpx.ConnectError:
         print("failed to connect the internet.")
